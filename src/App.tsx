852f1be2096e3f3dc1df79d6218e361739f1be8d
Dominik Rodler <dominik.rodler@gmail.com>
2022-03-27 17:07:16 +0200

    Mock interaction with video player (w/ YouTube)

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@
 import {memo} from 'react';
 import {ThemeProvider} from 'styled-components/macro';
 
-import {DrumMachine} from '~/modules/drum-machine';
+import {Learning} from '~/modules/learning';
 
 import {GlobalStyles, theme} from '~/styles';
 
 const App = memo(() => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <DrumMachine />
+    <Learning />
   </ThemeProvider>
 ));
 
---
+++ src/modules/learning/Learning.tsx
@@ -0,0 +1,71 @@
+import {useCallback, useState} from 'react';
+import YouTube from 'react-youtube';
+import styled from 'styled-components/macro';
+
+import {DrumMachine} from '~/modules/drum-machine';
+
+import {Box} from '~/components';
+
+import {pinTopLeft} from '~/styles';
+
+const VideoPlayer = styled(Box)<{collapsed: boolean}>`
+  ${pinTopLeft}
+
+  align-items: center;
+  aspect-ratio: 16/9;
+  background: ${({theme}) => theme.color.background.darkest};
+  justify-content: center;
+  padding: ${({theme}) => theme.size.default};
+  transform-origin: 0 0;
+  transition: all ${({theme}) => theme.transition.time.medium} ${({theme}) => theme.transition.style.dynamic}
+    ${({theme}) => theme.transition.time.slow};
+  z-index: ${({collapsed, theme}) => (collapsed ? theme.zIndex.base : theme.zIndex.overlaid1)};
+  width: 100%;
+
+  ${({collapsed}) =>
+    collapsed &&
+    `
+      aspect-ratio: 1/1;
+      padding: 0;
+      transform: scale(10%);
+      transition-delay: 0;
+    `}
+`;
+
+export const Learning = () => {
+  const [collapsed, toggleVideoPlayer] = useState(false);
+  const handlePlayback = useCallback(
+    (event: 'start' | 'stop' | 'end') => toggleVideoPlayer(['stop', 'end'].includes(event)),
+    []
+  );
+
+  const videoId = '4Qp7uI-ZNl8';
+
+  return (
+    <div>
+      <VideoPlayer collapsed={collapsed} onClick={() => toggleVideoPlayer(!collapsed)}>
+        <YouTube
+          videoId={videoId}
+          id="video-player" // defaults -> null
+          // className={string} // defaults -> null
+          // containerClassName={string} // defaults -> ''
+          // title={string} // defaults -> null
+          opts={{
+            height: '780',
+            width: '1280',
+          }}
+          // onReady={func} // defaults -> noop
+          onPlay={() => handlePlayback('start')}
+          onPause={() => handlePlayback('stop')}
+          onEnd={() => handlePlayback('end')}
+          // onError={func} // defaults -> noop
+          // onStateChange={func} // defaults -> noop
+          // onPlaybackRateChange={func} // defaults -> noop
+          // onPlaybackQualityChange={func} // defaults -> noop
+        />
+      </VideoPlayer>
+
+      <DrumMachine />
+    </div>
+  );
+};
